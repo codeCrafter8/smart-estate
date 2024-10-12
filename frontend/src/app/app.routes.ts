3cd49b43d1259e559ff52cae03cff95bc05b5ff9
zuzannawozniak <zuzanna.wk@interia.pl>
2024-10-12 16:19:49 +0200

    Create a Search Bar Component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { HomepageComponent } from './features/homepage/homepage.component';
 
 export const routes: Routes = [
+    { path: '', component: HomepageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 ];
