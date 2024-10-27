d0538b1318c272cfd3a17a83bd1ec54d393b5002
zuzannawozniak <zuzanna.wk@interia.pl>
2024-10-27 16:54:21 +0100

    Fix test

--- frontend/src/app/features/properties/components/property-advert/property-advert.component.spec.ts
+++ frontend/src/app/features/properties/components/property-advert/property-advert.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropertyAdvertComponent } from './property-advert.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PropertyAdvertComponent', () => {
   let component: PropertyAdvertComponent;
@@ -8,7 +9,8 @@ describe('PropertyAdvertComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PropertyAdvertComponent]
+      imports: [PropertyAdvertComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
