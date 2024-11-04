4bcaef0e50ac42657ce511a7da163c638cfbbaf4
zuzannawozniak <zuzanna.wk@interia.pl>
2024-11-04 19:26:37 +0100

    Fix test

--- frontend/src/app/features/properties/components/my-adverts/my-adverts.component.spec.ts
+++ frontend/src/app/features/properties/components/my-adverts/my-adverts.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyAdvertsComponent } from './my-adverts.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('MyAdvertsComponent', () => {
   let component: MyAdvertsComponent;
@@ -8,7 +9,8 @@ describe('MyAdvertsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyAdvertsComponent]
+      imports: [MyAdvertsComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
